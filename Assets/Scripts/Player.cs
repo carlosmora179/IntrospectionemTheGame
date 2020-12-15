af82d85e558445f075b0c858669312997f143be2
KEVIN <kevin150598@gmail.com>
2020-12-15 12:57:25 -0600

    Movimiento del personaje

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 7.5f;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.W)))
+        {
+            move.y = 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || (Input.GetKey(KeyCode.S)))
+        {
+            move.y = -1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A)))
+        {
+            move.x = -1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || (Input.GetKey(KeyCode.D)))
+        {
+            move.x = 1;
+        }
+
+        rb2d.MovePosition((Vector2)this.transform.position + (move * speed * Time.deltaTime));
+    }
+}
