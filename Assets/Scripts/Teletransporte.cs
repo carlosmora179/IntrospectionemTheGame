ce8be1cd74efcd6b6a14c2a9b25f31d6721b68c3
KEVIN <kevin150598@gmail.com>
2020-12-18 14:47:19 -0600

    Esperando la flecha

--- Assets/Scripts/Teletransporte.cs
+++ Assets/Scripts/Teletransporte.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Teletransporte : MonoBehaviour
 {
+    public GameObject target;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +13,11 @@ public class Teletransporte : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
     }
