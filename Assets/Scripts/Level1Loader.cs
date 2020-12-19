5a7323baafe1716957fa12868825f7cc3b5feb98
Carlos Mora <carlosmora179@gmail.com>
2020-12-18 22:06:47 -0600

    menu

---
+++ Assets/Scripts/Level1Loader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level1Loader : MonoBehaviour
+{
+    // Start is called before the first frame updatepublic void LoadNextLevel(){
+
+    public Animator transition;
+
+    public float transitionTime = 5f;
+
+ 
+    
+
+     public void LoadLevel1(){
+
+        if(SceneManager.GetActiveScene().buildIndex == 7){
+            StartCoroutine(LoadLevel(0));
+
+        }
+        else{
+
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        }
+        
+         
+
+
+    }
+    IEnumerator LoadLevel(int levelIndex){
+        //play
+            transition.SetTrigger("Jugar");
+        //wait
+        yield return new WaitForSeconds(transitionTime );
+
+        //load Scene
+        SceneManager.LoadScene(levelIndex);
+
+
+    }
+
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+   public Animator transition;
+
+   public GameObject canva;
+   public GameObject gameOver;
+
+   [Tooltip("Mundo al que pertenece el player")]
+    public int mundoInicio;
+
+   public float transitionTime = 1f;
+    // Update is called once per frame
+   private void Start() {
+        StartCoroutine(loadInterface());
+       
+   }
+   public void GameOver(){
+
+       Debug.Log("perdio puto");
+      StartCoroutine(finJuego());
+
+   }
+   public void ReinicioMundo(){
+
+       SceneManager.LoadScene(mundoInicio);
+   }
+
+    public void LoadNextLevel(){
+
+        
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+         
+
+
+    }
+    IEnumerator LoadLevel(int levelIndex){
+        //play
+            transition.SetTrigger("Start");
+        //wait
+        yield return new WaitForSeconds(transitionTime );
+
+        //load Scene
+        SceneManager.LoadScene(levelIndex);
+
+
+    }
+
+   IEnumerator loadInterface()
+  {
+       
+       yield return new WaitForSeconds(transitionTime);
+       canva.SetActive(true);
+  }
+  IEnumerator finJuego()
+  {
+       canva.SetActive(false);
+       gameOver.SetActive(true);
+       yield return new WaitForSeconds(2f);
+  }
+}
