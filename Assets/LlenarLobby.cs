758d6050e8da11868e24d418188f1a9cdf6579b1
Carlos Mora <carlosmora179@gmail.com>
2020-12-18 21:14:06 -0600

    se agregaron las capas daniadas

---
+++ Assets/LlenarLobby.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LlenarLobby : MonoBehaviour
+{
+    
+    [SerializeField]
+     GameObject danio0,danio1,danio2,danio3;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+
+     public void limpiarLobby(){
+         danio0.SetActive(true);
+        danio1.SetActive(false);
+
+    }
+    public void llenarPadre(){
+        danio2.SetActive(true);
+
+    }
+    public void llenarHermana(){
+        danio3.SetActive(true);
+
+    }
+}
